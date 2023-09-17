5a6737c373c28aff82907d130dc2c0547e4d957a
Dilesh Madhavan <madhavandilesh@gmail.com>
2023-09-17 16:31:25 +0530

    Scoreboard implemented,gameboard & reset functionality done

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -9,8 +9,22 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  String result = '';
+  bool oTurn = true;
+  List<String> displayXO = ['', '', '', '', '', '', '', '', ''];
+  int oScore = 0;
+  int xScore = 0;
+  int filledBoxes = 0;
   static var customFontWhite = GoogleFonts.oxygen(
     textStyle: const TextStyle(
+      color: Colors.white,
+      letterSpacing: 5,
+      fontSize: 26,
+    ),
+  );
+  static var customStyle = GoogleFonts.oxygen(
+    textStyle: const TextStyle(
+      decoration: TextDecoration.underline,
       color: Colors.white,
       letterSpacing: 5,
       fontSize: 26,
@@ -25,14 +39,47 @@ class _GameScreenState extends State<GameScreen> {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
+              // ScoreBoard
               Expanded(
-                child: Center(
-                  child: Text(
-                    'Score Borad',
-                    style: customFontWhite,
-                  ),
+                  child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text('PLAYER O', style: customStyle),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                          oScore.toString(),
+                          style: customFontWhite,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'PLAYER X',
+                          style: customStyle,
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                          xScore.toString(),
+                          style: customFontWhite,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
+              )),
+
+              // GameBoard
               Expanded(
                 flex: 3,
                 child: GridView.builder(
@@ -42,7 +89,7 @@ class _GameScreenState extends State<GameScreen> {
                   itemBuilder: (BuildContext context, index) {
                     return GestureDetector(
                       onTap: () {
-                        print('click');
+                        tapped(index);
                       },
                       child: Container(
                         decoration: BoxDecoration(
@@ -54,7 +101,7 @@ class _GameScreenState extends State<GameScreen> {
                             color: Colors.orange.shade400),
                         child: Center(
                           child: Text(
-                            'O',
+                            displayXO[index],
                             style: GoogleFonts.lilitaOne(
                                 textStyle: const TextStyle(
                               color: Colors.white,
@@ -69,12 +116,151 @@ class _GameScreenState extends State<GameScreen> {
               ),
               Expanded(
                 flex: 2,
-                child: Center(child: Text('Timer', style: customFontWhite)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        reset();
+                      },
+                      child: Text('Reset Board'),
+                    ),
+                    Text(
+                      result,
+                      style: customFontWhite,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        newGame();
+                      },
+                      child: Text('Start a new Game'),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  void newGame() {
+    reset();
+    oScore = 0;
+    xScore = 0;
+  }
+
+  void reset() {
+    setState(() {
+      displayXO = ['', '', '', '', '', '', '', '', ''];
+      result = '';
+      filledBoxes = 0;
+    });
+  }
+
+  void tapped(int index) {
+    setState(() {
+      if (oTurn && displayXO[index] == '') {
+        displayXO[index] = 'O';
+        filledBoxes++;
+      } else if (displayXO[index] == '') {
+        displayXO[index] = 'X';
+        filledBoxes++;
+      }
+      oTurn = !oTurn;
+      checkwinner();
+    });
+  }
+
+  void checkwinner() {
+    //check 1st row//
+    if (displayXO[0] == displayXO[1] &&
+        displayXO[0] == displayXO[2] &&
+        displayXO[0] != '') {
+      setState(() {
+        result = 'Player ${displayXO[0]} Wins!';
+        updateScore(displayXO[0]);
+      });
+    }
+    //check 2nd row//
+    if (displayXO[3] == displayXO[4] &&
+        displayXO[3] == displayXO[5] &&
+        displayXO[3] != '') {
+      setState(() {
+        result = 'Player ${displayXO[3]} Wins!';
+        updateScore(displayXO[3]);
+      });
+    }
+    //check 3rd row//
+    else if (displayXO[6] == displayXO[7] &&
+        displayXO[6] == displayXO[8] &&
+        displayXO[6] != '') {
+      setState(() {
+        result = 'Player ${displayXO[6]} Wins!';
+        updateScore(displayXO[6]);
+      });
+    }
+    //check 1st column//
+    else if (displayXO[0] == displayXO[3] &&
+        displayXO[0] == displayXO[6] &&
+        displayXO[0] != '') {
+      setState(() {
+        result = 'Player ${displayXO[0]} Wins!';
+        updateScore(displayXO[0]);
+      });
+    }
+    //check 2nd column//
+    else if (displayXO[1] == displayXO[4] &&
+        displayXO[1] == displayXO[7] &&
+        displayXO[1] != '') {
+      setState(() {
+        result = 'Player ${displayXO[1]} Wins!';
+        updateScore(displayXO[1]);
+      });
+    }
+    //check 3rd column//
+    else if (displayXO[2] == displayXO[5] &&
+        displayXO[2] == displayXO[8] &&
+        displayXO[2] != '') {
+      setState(() {
+        result = 'Player ${displayXO[2]} Wins!';
+        updateScore(displayXO[2]);
+      });
+    }
+
+    //check diagonal//
+    else if (displayXO[0] == displayXO[4] &&
+        displayXO[0] == displayXO[8] &&
+        displayXO[0] != '') {
+      setState(() {
+        result = 'Player ${displayXO[0]} Wins!';
+        updateScore(displayXO[0]);
+      });
+    }
+
+    //check diagonal//
+    else if (displayXO[2] == displayXO[4] &&
+        displayXO[2] == displayXO[6] &&
+        displayXO[2] != '') {
+      setState(() {
+        result = 'Player ${displayXO[2]} Wins!';
+        updateScore(displayXO[2]);
+      });
+    } else if (filledBoxes == 9) {
+      setState(() {
+        result = 'Match tied !';
+      });
+    }
+  }
+
+  void updateScore(String winner) {
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
+      xScore++;
+    } else {
+      return;
+    }
   }
 }
